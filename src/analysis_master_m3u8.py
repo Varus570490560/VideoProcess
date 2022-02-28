e6127e95db356e9f3544c8401f5473f5a83cbaef
XuShuo <xushuo@highlight.mobi>
2022-02-28 16:16:40 +0800

    根据输入文件获取视频

--- src/analysis_master_m3u8.py
+++ src/analysis_master_m3u8.py
@@ -3,22 +3,12 @@ import config_parse
 import requests
 
 headers = {
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "accept-language": "zh-CN,zh;q=0.9",
-    "sec-ch-ua": "\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"97\", \"Chromium\";v=\"97\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"macOS\"",
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "none",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1",
-    "cookie": "__gads=ID=79b966ca278ddd0f:T=1643106054:S=ALNI_MZMzQxTkW35RLEkPhHODyRb_nAeIw; _ga=GA1.2.814802378.1643106054; noauth=1643106060522; _fbp=fb.1.1643106063270.17137962; zdbb_fb_id=1; fpid=250a51daaf6f4816af45438be2706d56; _pubcid=896a8077-214d-4755-bc6b-84cce426dcf0; _cc_id=5f117e62fccbf9f29af3e4c66d08944b; _gaexp=GAX1.2.1aGFJQwZQuqGI9ZgmkGyaQ.19123.0; geoCC=HK; _gid=GA1.2.1580160982.1645511246; panoramaId_expiry=1646116071305; panoramaId=02d5d22dfb13a18eb50fa77759b516d53938181e044a5b09af2d7adb82b768e3; comScore=amp-rCBvNUCUt2Psl7vSjFNCuA; crid=MjUwYTUxZGFhZjZmNDgxNmFmNDU0MzhiZTI3MDZkNTZ8MDM5Y2MwMTItYjU5OS00MDM2LWJhMjItZTQyZTZmZTBhYmE3; __idcontext=eyJjb29raWVJRCI6IkRBRDczN0k0U1ZPNlVVTzZQRUpPRkNCU0JJQjNJT1c3TVBLU0dQNUJTTkVRPT09PSIsImRldmljZUlEIjoiREFENzM3STRTWkc0RzdQNUxZSTQzU0FVQ0E1Sk9MV09NVExSV001NDJKRVE9PT09IiwiaXYiOiJFWkxJU0FaS1lBN1RKTkpDVzZSMjJMNllUQT09PT09PSIsInYiOjF9; FCNEC=[[\"AKsRol9INEFhHjpd6ivA32eyeLlnolatNIM5ALkV4eIpBQouGQrgYXMQKvAa6cj0il__CRh7EwuPhwQFhB-6b8IT9IdDt4nlt1PEMLkvsZfLF5Rox1zTSYTpwkkqmq-CPlrU4-uzlWuJZQE7FLKCO3504c7JkS5aOQ==\"],null,[]]; cto_bundle=Ug4QN19yc0JneUxxZnpyeU1mZkFQR1IlMkJjbXNMYmdlSHNhaCUyQk8lMkJqdElOUUVyckRvcWZBSmFwdTNRZzgxTzZ5T3pBVjgxJTJGYThwdzF3UzJRMXFhNnNxZVZXYXdqWWtSNHY5bVdPQ0xuTEF4cGZUNnptR1lQN2d5ckpkbUtycFZCJTJGT0JOTCUyRmVJY3ZVbVNmTnVtWjVRZmtXVGNUT0ElM0QlM0Q; cto_bidid=dpt9xV9QWG1VZktKV3J5Z2FROXJPMVlTekFFRzhnOW1zcFEzbWRCMHFNdDVVOTZhZWR1VURvZm1jcFIyQk1WcWdsSUNZa0NSeG9uWVpScmY5Vks0cU4lMkJRTmMwMGFCUE1vRlZvc2JYbXRiTjBNdU5RJTNE"
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 '
 }
 
 
 def analysis_master():
-    with open('./input/master.m3u8', 'r') as master:
+    with open('./input/ccb26c930feed7d458f3ed20ba562be7_master.m3u8', 'r') as master:
         while True:
             line = master.readline()
             if not line:
@@ -82,9 +72,9 @@ def analysis_m3u8():
 def generate_video_list():
     for root, dirs, files in os.walk(config_parse.get_path() + '/VideoProcess/m3u8'):
         for file in files:
-            with open(config_parse.get_path()+'/VideoProcess/m3u8/'+file, 'r') as r:
+            with open(config_parse.get_path() + '/VideoProcess/m3u8/' + file, 'r') as r:
                 domain_url = find_domain(remove_new_line(r.readline()))
-                with open(config_parse.get_path()+'/VideoProcess/video_list/'+file,'wb') as w:
+                with open(config_parse.get_path() + '/VideoProcess/video_list/' + file, 'wb') as w:
                     while True:
                         line = r.readline()
                         if not line:
@@ -92,7 +82,8 @@ def generate_video_list():
                         if line[0] == '#':
                             continue
                         else:
-                            input = ("file '" + config_parse.get_path() + "/VideoProcess/ts/" +find_name(file)+'/' + remove_new_line(line) + "'").encode()
+                            input = ("file '" + config_parse.get_path() + "/VideoProcess/ts/" + find_name(
+                                file) + '/' + remove_new_line(line) + "'").encode()
                             w.write(input)
                             w.write('\n'.encode())
     print('Generate video list completely!!!')
@@ -100,3 +91,22 @@ def generate_video_list():
 
 def remove_new_line(input):
     return input[:len(input) - 1]
+
+
+def url_to_master():
+    with open('./url/url.txt', 'r') as urls:
+        while True:
+            url = urls.readline()
+            if not url:
+                break
+            else:
+                url = url[:len(url) - 1]
+                response = requests.get(url=url, headers=headers)
+                print(response)
+                response_json = response.json()
+                master_url = response_json['data']['videoPlayerProps']['refs']['m3uUrl']
+                name = response_json['data']['videoPlayerProps']['id']
+                master_response = requests.get(url=master_url, headers=headers)
+                print(master_response)
+                with open('./input/' + name + '_master.m3u8', 'wb') as master:
+                    master.write(master_response.content)
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import os
 import analysis_master_m3u8
 import config_parse
 if __name__ == '__main__':
+    analysis_master_m3u8.url_to_master()
     analysis_master_m3u8.analysis_master()
     analysis_master_m3u8.analysis_m3u8()
     analysis_master_m3u8.generate_video_list()
