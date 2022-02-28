7f84d2e76d6f1f0006ce2b668bc4d27f2c6366c3
XuShuo <xushuo@highlight.mobi>
2022-02-28 17:17:58 +0800

    多个视频处理

--- src/analysis_master_m3u8.py
+++ src/analysis_master_m3u8.py
@@ -8,22 +8,24 @@ headers = {
 
 
 def analysis_master():
-    with open('./input/ccb26c930feed7d458f3ed20ba562be7_master.m3u8', 'r') as master:
-        while True:
-            line = master.readline()
-            if not line:
-                break
-            if line[0] == '#':
-                continue
-            else:
-                line = line[:len(line) - 1]
-                response = requests.get(url=line, headers=headers, allow_redirects=True)
-                with open(config_parse.get_path() + '/VideoProcess/m3u8/' + find_file_name(line), 'wb') as out:
-                    out.write(line.encode())
-                    out.write('\n'.encode())
-                    out.write(response.content.decode('utf8').encode())
-                    break
-    print('master analysis completely!!!')
+    for root, dirs, files in os.walk(config_parse.get_path() + '/VideoProcess/input/'):
+        for file in files:
+            with open('./input/'+file, 'r') as master:
+                while True:
+                    line = master.readline()
+                    if not line:
+                        break
+                    if line[0] == '#':
+                        continue
+                    else:
+                        line = line[:len(line) - 1]
+                        response = requests.get(url=line, headers=headers, allow_redirects=True)
+                        with open(config_parse.get_path() + '/VideoProcess/m3u8/' + find_file_name(line), 'wb') as out:
+                            out.write(line.encode())
+                            out.write('\n'.encode())
+                            out.write(response.content.decode('utf8').encode())
+                            break
+            print('master analysis completely!!!')
 
 
 def find_file_name(url):
