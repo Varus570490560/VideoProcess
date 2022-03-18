dbef2e21796c5ba862cf05998c03c156c708e166
XuShuo <xushuo@highlight.mobi>
2022-03-18 11:04:23 +0800

    fix

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
 run:
 	python ./src/main.py
+	make clear
 merge_video:
 	./script/merge_video.sh
 clear:
-	rm ./input/*;
 	rm ./m3u8/*;
-	rm ./output/*;
-	rm -r ./ts/*;
+	rm -rf ./ts/*;
 	rm ./video_list/*;
+generate:
+	mkdir inputa m3u8 output ts url video_list
--- src/analysis_master_m3u8.py
+++ src/analysis_master_m3u8.py
@@ -10,7 +10,7 @@ headers = {
 def analysis_master():
     for root, dirs, files in os.walk(config_parse.get_path() + '/VideoProcess/input/'):
         for file in files:
-            with open('./input/'+file, 'r') as master:
+            with open('./input/' + file, 'r') as master:
                 while True:
                     line = master.readline()
                     if not line:
@@ -25,7 +25,7 @@ def analysis_master():
                             out.write('\n'.encode())
                             out.write(response.content.decode('utf8').encode())
                             break
-            print('master analysis completely!!!')
+            print(file, 'master analysis completely!!!')
 
 
 def find_file_name(url):
@@ -48,12 +48,25 @@ def find_domain(url):
     return url[:n]
 
 
+def find_end(file_point):
+    i = 0
+    while True:
+        line = file_point.readline()
+        if not line:
+            return int(i - 7) / 2
+        else:
+            i = i + 1
+
+
 def analysis_m3u8():
     for root, dirs, files in os.walk(config_parse.get_path() + '/VideoProcess/m3u8'):
         for file in files:
             if not os.path.exists(config_parse.get_path() + '/VideoProcess/ts/' + find_name(file)):
                 os.mkdir(config_parse.get_path() + '/VideoProcess/ts/' + find_name(file))
             with open(config_parse.get_path() + '/VideoProcess/m3u8/' + file, 'r') as m:
+                total = find_end(m)
+                i = 0
+                m.seek(0, 0)
                 base = find_domain(m.readline())
                 while True:
                     line = m.readline()
@@ -62,10 +75,10 @@ def analysis_m3u8():
                     if line[0] == '#':
                         continue
                     else:
+                        i = i + 1
                         line = line[:len(line) - 1]
                         response = requests.get(url=base + '/' + line)
-                        print(base + line)
-                        print(response)
+                        print(line+' downloading:'+str(i)+'/'+str(total))
                         with open(config_parse.get_path() + '/VideoProcess/ts/' + find_name(file) + '/' + line,
                                   'wb') as w:
                             w.write(response.content)
@@ -104,11 +117,13 @@ def url_to_master():
             else:
                 url = url[:len(url) - 1]
                 response = requests.get(url=url, headers=headers)
-                print(response)
                 response_json = response.json()
                 master_url = response_json['data']['videoPlayerProps']['refs']['m3uUrl']
                 name = response_json['data']['videoPlayerProps']['id']
                 master_response = requests.get(url=master_url, headers=headers)
-                print(master_response)
+                if master_response.status_code == 200:
+                    print(master_url, '\nget master file successfully!')
+                else:
+                    print(master_url, 'get master file failed! http status code =', master_response.status_code)
                 with open('./input/' + name + '_master.m3u8', 'wb') as master:
                     master.write(master_response.content)
